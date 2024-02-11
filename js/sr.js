521239bce1fdc89c1c3f1a95a5582ff5e7f7efb9
Gabriel Moura <100838337+musgodev@users.noreply.github.com>
2024-02-11 07:01:32 -0300

    Add files via upload

---
+++ js/sr.js
@@ -0,0 +1,9 @@
+const sr = ScrollReveal({
+    origin: 'left',
+    distance: '50px',
+    duration: 2000,
+    reset: true
+});
+
+ScrollReveal().reveal('.icon-img', {  distance: '10px', delay: 100, origin: 'top', });
+ScrollReveal().reveal('.card-unit', { delay: 100, distance: '400px' });
